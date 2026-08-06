2eb22049d28560d4d22849b102f57027cdef15ee
1756660639 <56524052+1756660639@users.noreply.github.com>
2026-08-06 15:32:36 +0800

    Update com.netease.cloudmusic.ts

--- src/apps/com.netease.cloudmusic.ts
+++ src/apps/com.netease.cloudmusic.ts
@@ -1,21 +1,20 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'com.netease.cloudmusic',
-  name: '网易云音乐测试',
-  groups: [
+  "id": "com.netease.cloudmusic",
+  "name": "网易云音乐测试",
+  "groups": [
     {
-      key: 99,
-      name: '底部免费听横幅',
-      fastQuery: true,
-      rules: [
+      "key": 99,
+      "name": "底部免费听横幅",
+      "fastQuery": true,
+      "rules": [
         {
-          key: 0,
-          activityIds: '.activity.MainActivity',
-          matches:
-            "ImageView[id='com.netease.cloudmusic:id/closeIV'][clickable=true][visibleToUser=true]",
-        },
-      ],
-    },
-  ],
+          "key": 0,
+          "activityIds": ".activity.MainActivity",
+          "matches": "ImageView[id=\"com.netease.cloudmusic:id/closeIV\"][clickable=true][visibleToUser=true]"
+        }
+      ]
+    }
+  ]
 });
